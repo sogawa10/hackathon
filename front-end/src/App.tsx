c2445338af5f8b405aa4dc3748dd885bd36d6d99
KatsuhiroAbe <abo0107kyabe@outlook.jp>
2026-05-28 16:18:11 +0900

    籠ページの作成

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import HarvestBasket from './pages/HarvestBasket';
 
 const App: React.FC = () => {
   return (
@@ -11,7 +12,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* 他のページが増えたらここに追加します */}
+        <Route path="/basket" element={<HarvestBasket />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
